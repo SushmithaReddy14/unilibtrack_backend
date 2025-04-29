adf6c0a73f80c7e08bc11c978f53232ee22ec681
Sushmitha Reddy <srx98420@ucmo.edu>
2025-04-29 00:19:38 -0500

    allowing multiple cross origin urls

--- Lib/app.py
+++ Lib/app.py
@@ -11,12 +11,21 @@ from routes.auth_routes import auth_bp
 app = Flask(__name__)
 app.secret_key = 'ff#%455hjbk'
 
-allowed_origins = [
-    "http://localhost:3000",  
-    "https://unilibtrack-frontend.onrender.com"
-]
-
-CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": allowed_origins}},)
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "https://unilibtrack-frontend.onrender.com",
+            "http://localhost:3000"
+        ],
+        "supports_credentials": True,
+        "allow_headers": [
+            "Content-Type",
+            "Authorization",
+            "Access-Control-Allow-Credentials",
+            "Access-Control-Allow-Origin"
+        ]
+    }
+})
 
 app.register_blueprint(user_bp, url_prefix="/users")
 app.register_blueprint(book_bp, url_prefix="/books")
